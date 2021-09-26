64b445abc41d0c8c30474b9e2dcf830bad18a80f
kelvinyhenrique <kelvinyhenrique17@gmail.com>
2021-09-25 23:22:28 -0300

    Fix

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(require('./Routes'));
+app.use(require('./routes'));
 
 dotenv.config();
 
--- src/routes/Hook.ts
+++ src/routes/Hook.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import HookController from '../Controllers/HookController';
+import HookController from '../controllers/HookController';
 
 const router = express.Router();
 
